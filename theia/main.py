f3835fa601703c4cff9906878a6611ecaf346b06
o-gent <olivergent@btinternet.com>
2021-12-20 23:29:45 +0000

    import fixes

--- theia/main.py
+++ theia/main.py
@@ -1,9 +1,9 @@
 from typing import List, Tuple
 import numpy as np
 from theia.spec import ImageRecognitionResult
-from utils import logger
+from theia.utils import logger
 
-def image_recognition(image: np.ndarray,  gps_image_taken: Tuple(float, float)) -> List[ImageRecognitionResult]:
+def image_recognition(image: np.ndarray,  gps_image_taken: Tuple[float, float]) -> List[ImageRecognitionResult]:
     """
     The resulting function of all this mess
     """
