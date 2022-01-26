acdec7cf3236351e45c13e80d3b5241a1eb16f6e
o-gent <olivergent@btinternet.com>
2022-01-26 14:58:23 +0000

    some tidying

--- dataset/generate.py
+++ dataset/generate.py
@@ -1,3 +1,8 @@
+""" 
+Messy file needed in generate.ipynb for multiprocessing as well as some key functions for image manipulation
+"""
+
+
 
 import cv2
 import numpy as np
@@ -60,3 +65,128 @@ def create_transform_images(file):
             #utils.display(marked)
             r.append(f"{name}, {x}, {y} \n")
     return r
+
+
+def rotate_bound(image, angle):
+    # grab the dimensions of the image and then determine the
+    # center
+    (h, w) = image.shape[:2]
+    (cX, cY) = (w // 2, h // 2)
+
+    # grab the rotation matrix (applying the negative of the
+    # angle to rotate clockwise), then grab the sine and cosine
+    # (i.e., the rotation components of the matrix)
+    M = cv2.getRotationMatrix2D((cX, cY), -angle, 1.0)
+    cos = np.abs(M[0, 0])
+    sin = np.abs(M[0, 1])
+
+    # compute the new bounding dimensions of the image
+    nW = int((h * sin) + (w * cos))
+    nH = int((h * cos) + (w * sin))
+
+    # adjust the rotation matrix to take into account translation
+    M[0, 2] += (nW / 2) - cX
+    M[1, 2] += (nH / 2) - cY
+
+    # perform the actual rotation and return the image
+    rotated_mat = cv2.warpAffine(image, M, (nW, nH))
+    
+    tmp = cv2.cvtColor(rotated_mat, cv2.COLOR_RGB2GRAY)
+    _,alpha = cv2.threshold(tmp,0,255,cv2.THRESH_BINARY)
+    b, g, r = cv2.split(rotated_mat)
+    rgba = [b,g,r,alpha]
+    dst = cv2.merge(rgba,4)
+    
+    return dst
+
+
+def add_gaussian_noise(X_imgs):
+    gaussian_noise_imgs = []
+    row, col, _ = X_imgs[0].shape
+    # Gaussian distribution parameters
+    mean = 0
+    var = 0.1
+    sigma = var ** 0.5
+    
+    for X_img in X_imgs:
+        gaussian = np.random.random((row, col, 1)).astype(np.float32)
+        gaussian = np.concatenate((gaussian, gaussian, gaussian), axis = 2)
+        gaussian_img = cv2.addWeighted(X_img, 0.75, 0.25 * gaussian, 0.25, 0)
+        gaussian_noise_imgs.append(gaussian_img)
+    gaussian_noise_imgs = np.array(gaussian_noise_imgs, dtype = np.float32)
+    return gaussian_noise_imgs
+
+def noise(img):
+    mean = 0
+    var = 10
+    sigma = var ** 0.5
+    gaussian = np.random.normal(mean, sigma, (100, 100)) #  np.zeros((224, 224), np.float32)
+    noisy_image = np.zeros(img.shape, np.float32)
+    
+
+    if len(img.shape) == 2:
+        noisy_image = img + gaussian
+    else:
+        noisy_image[:, :, 0] = img[:, :, 0] + gaussian
+        noisy_image[:, :, 1] = img[:, :, 1] + gaussian
+        noisy_image[:, :, 2] = img[:, :, 2] + gaussian
+
+    cv2.normalize(noisy_image, noisy_image, 0, 255, cv2.NORM_MINMAX, dtype=-1)
+    noisy_image = noisy_image.astype(np.uint8)
+    return noisy_image
+
+
+def increase_brightness(img, value=30):
+    """ ??? %$&* """
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv)
+
+    lim = 255 - (value if value > 0 else 0)
+    v[v > lim] = 255
+    #v[v < lim] = 0
+    #v[v <= lim] += value
+    v[v >= lim] -= value
+
+    final_hsv = cv2.merge((h, s, v))
+    img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+    return img
+
+
+def imageOverlay(background, overlay, x, y, width):
+    """ overlay an image and set the width of the overlay """
+    
+    dim = (width, width)
+    # resize image
+    overlay = cv2.resize(overlay, dim, interpolation = cv2.INTER_AREA)
+
+    background_width = background.shape[1]
+    background_height = background.shape[0]
+
+    if x >= background_width or y >= background_height:
+        return background
+
+    h, w = overlay.shape[0], overlay.shape[1]
+
+    if x + w > background_width:
+        w = background_width - x
+        overlay = overlay[:, :w]
+
+    if y + h > background_height:
+        h = background_height - y
+        overlay = overlay[:h]
+
+    if overlay.shape[2] < 4:
+        overlay = np.concatenate(
+            [
+                overlay,
+                np.ones((overlay.shape[0], overlay.shape[1], 1), dtype = overlay.dtype) * 255
+            ],
+            axis = 2,
+        )
+
+    overlay_image = overlay[..., :3]
+    mask = overlay[..., 3:] / 255.0
+
+    background[y:y+h, x:x+w] = (1.0 - mask) * background[y:y+h, x:x+w] + mask * overlay_image
+
+    return background
