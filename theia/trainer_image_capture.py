a97d869a2a3f8fba95e0ab9be75d3c89fc0c5b59
pmsem <25342341+pmsem@users.noreply.github.com>
2022-03-17 14:19:37 +0000

    draft of trainer image capture code before testing

---
+++ theia/trainer_image_capture.py
@@ -0,0 +1,18 @@
+from picamera2.picamera2 import *
+import time
+from dronekit import connect
+
+picam2 = Picamera2()
+vehicle = connect('network address', wait_ready=True) #(not sure how we're connecting to the pi)
+
+fps = 5 # how many fps to record
+
+picam2.start()
+time.sleep(2)
+
+while(True): #change condition to something like "while armed" using dronekit. or use vehicle mission attribute for working with waypoints? 
+    time.sleep(1/fps) #check units
+    location = str(vehicle.location.global_frame) #check if this actually yields coorect gps coordinates
+    metadata = picam2.capture_file(location+".jpg")
+
+picam2.close()
