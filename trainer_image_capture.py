bc4b9ecfd7a4ae2a842cc4baa78d90a4438c997b
o-gent <olivergent@btinternet.com>
2022-03-17 18:36:25 +0000

    some adjustments

---
+++ trainer_image_capture.py
@@ -0,0 +1,30 @@
+from picamera2.picamera2 import *
+import time
+from dronekit import connect
+
+picam2 = Picamera2()
+vehicle = connect('/dev/USB', wait_ready=True) #(not sure how we're connecting to the pi)
+
+print(vehicle)
+print(picam2)
+
+config = picam2.preview_configuration() #these 3 lines should turn off preview
+picam2.configure(config)
+picam2.start_preview(Preview.NULL)
+
+fps = 5 # how many fps to record
+
+picam2.start()
+time.sleep(2)
+
+try:
+    while(True): #change condition to something like "while armed" using dronekit. or use vehicle mission attribute for working with waypoints? 
+        time.sleep(1/fps) #check units
+        location = str(vehicle.location.global_frame).strip("LocationGlobal") #check if this actually yields coorect gps coordinates. cant really test without pixhawk
+        metadata = picam2.capture_file(location+".jpg")
+except KeyboardInterrupt:
+    print("keyboard interupt")
+except Exception:
+    print("exception")
+
+picam2.close()
