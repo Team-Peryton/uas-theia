a588f826d3a54d0ae0aadb55028f13d0cdf5db42
pmsem <25342341+pmsem@users.noreply.github.com>
2022-07-08 09:52:55 +0100

    pi changes

--- runme.py
+++ runme.py
@@ -2,22 +2,21 @@ from theia.main import ImageRecognition
 
 ir = ImageRecognition("/media/pi/USB DISK")
 
-try:
-    while True:
-        while True:
-            ir.image_recognition(ir.get_image(), ir.get_location_info())        
-        
+
+while True:
+    try:
+        ir.image_recognition(ir.get_image(), ir.get_location_info())
+
+    except KeyboardInterrupt:
+        print("keyboard interupt")
         ir.calaculate_target_position()
         ir.get_found_targets()
 
-except KeyboardInterrupt:
-    print("keyboard interupt")
-    ir.calaculate_target_position()
-    ir.get_found_targets()
+    except Exception as e:
+        print(e)
+        ir.calaculate_target_position()
+        ir.get_found_targets()
 
-except Exception as e:
-    print(e)
-    ir.calaculate_target_position()
-    ir.get_found_targets()
-
+ir.calculate_target_position()
+ir.get_found_targets()
 ir.picam.close()
